3b0e2f28ce8d975d985b3316b8c783ef24706560
Brad Rumack <bradrumack@Brads-Mac-mini.local>
2021-04-18 18:30:13 -0700

    updates

--- src/lib/move_file.js
+++ src/lib/move_file.js
@@ -1,13 +1,12 @@
 async function moveFile(fileId, previousParent, newParent) {
 
     const update = {
-        fileId,
         addParents: newParent,
         removeParents: previousParent
     }
 
     try {
-        return await this.drive.files.update({ update, fields: '*' })
+        return await this.drive.files.update({ fileId }, { update, fields: '*' })
     } catch (e) {
         throw new Error(e)
     }
