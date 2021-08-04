d58a372cafeeee410a3aef9630516f030f9b918e
brad <brad.rumack@gmail.com>
2021-08-03 23:26:01 -0700

    bug fix

--- src/lib/copy_file.js
+++ src/lib/copy_file.js
@@ -2,7 +2,7 @@ async function copyFile(fileId, parentFolder) {
     const drive = await this.createDrive()
     
     try {
-        const result = await drive.files.copy({ fileId, parents: [parentFolder], fields: '*' })
+        const result = await drive.files.copy({ fileId, requestBody: { parents: [parentFolder] }, fields: '*' })
         return result
     } catch(e) {
         throw new Error(e)
